9dd50d48ef2ccea31af2c4d68da26b34d3f8aefc
Durante101 <durthornton@gmail.com>
2025-06-09 21:44:46 -0400

    NorthwindTraders

---
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = "com.mys"
+    }
+}
